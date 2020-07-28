c22782bd90b21d40997414189f39df326399b4c6
Athar Zafeer <atharzafeer@outlook.com>
2020-07-28 20:18:02 +0530

    add sdcard interface

--- SABOT.ino
+++ SABOT.ino
@@ -1,6 +1,12 @@
 #define DEBUG 1
+/*-----Libraries-------*/
+/***Temperature Sensor*/
 #include<OneWire.h>
 #include<DallasTemperature.h>
+/***SDCARD*/
+#include "FS.h"
+#include "SD.h"
+#include "SPI.h"
 /*---variables and objects---*/
 //general variable declaration
 int i;
@@ -19,7 +25,45 @@ DeviceAddress batAddress,ucpAddress,cpAddress;
 const int address[]={0x33,0x81,0xF9};
 //
 /*--------FUNCTIONS-----------*/
+//SD card functions READ/WRITE
+void createDir(fs::FS &fs, const char * path){
+    Serial.printf("Creating Dir: %s\n", path);
+    if(fs.mkdir(path)){
+        Serial.println("Dir created");
+    } else {
+        Serial.println("mkdir failed");
+    }
+}
+void writeFile(fs::FS &fs, const char * path, const char * message){
+    Serial.printf("Writing file: %s\n", path);
 
+    File file = fs.open(path, FILE_WRITE);
+    if(!file){
+        Serial.println("Failed to open file for writing");
+        return;
+    }
+    if(file.print(message)){
+        Serial.println("File written");
+    } else {
+        Serial.println("Write failed");
+    }
+    file.close();
+}
+void appendFile(fs::FS &fs, const char * path, const char * message){
+    Serial.printf("Appending to file: %s\n", path);
+
+    File file = fs.open(path, FILE_APPEND);
+    if(!file){
+        Serial.println("Failed to open file for appending");
+        return;
+    }
+    if(file.print(message)){
+        Serial.println("Message appended");
+    } else {
+        Serial.println("Append failed");
+    }
+    file.close();
+}
 
 //print Temperature device address
 void printAddress(DeviceAddress deviceAddress)
@@ -119,12 +163,40 @@ float currentFunction(int GPIO){
   }
 void setup() {
   // put your setup code here, to run once:
-pinMode(34, INPUT);
-pinMode(33, INPUT);
-pinMode(35, INPUT);
-pinMode(25, INPUT);
-pinMode(26, INPUT);
-sensors.begin();
+  pinMode(34, INPUT);
+  pinMode(33, INPUT);
+  pinMode(35, INPUT);
+  pinMode(25, INPUT);
+  pinMode(26, INPUT);
+  sensors.begin();
+
+//SDCARD CODE START
+  if(!SD.begin()){
+    Serial.println("Card Mount Failed");
+    return;
+   }
+  uint8_16 cardType = SD.cardType();
+    if(cardType == CARD_NONE){
+        Serial.println("No SD card attached");
+        return;
+    }
+
+    Serial.print("SD Card Type: ");
+    if(cardType == CARD_MMC){
+        Serial.println("MMC");
+    } else if(cardType == CARD_SD){
+        Serial.println("SDSC");
+    } else if(cardType == CARD_SDHC){
+        Serial.println("SDHC");
+    } else {
+        Serial.println("UNKNOWN");
+    }
+
+    uint64_t cardSize = SD.cardSize() / (1024 * 1024);
+    Serial.printf("SD Card Size: %lluMB\n", cardSize);
+    WriteFile(SD, "/time.csv", "\tPower_ON\t\n");
+    appendFile(SD,"/time.csv", "Starting time after_interval");
+//SDCARD CODE END
 }
 
 void loop() {
